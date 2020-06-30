9d69b530a5d71f37c16ab6fb6e7f606214a1bdbe
Mohammed <momovit2222@gmail.com>
2020-06-30 23:50:10 +0200

    new commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory( \App\User::class , 50 )->create();
+
     }
 }
